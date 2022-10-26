3275f3ed7575d350f39e557e84c7c98599dfb4f0
amicuswat <amicuswat@gmail.com>
2022-10-26 11:37:14 +0300

    unapplied Flat.owner_pure_phone migration

--- property/migrations/0011_auto_20221026_0936.py
+++ property/migrations/0011_auto_20221026_0936.py
@@ -13,11 +13,11 @@ def fill_phones(apps, schema_editor):
         flat.save()
 
 
-# def move_backwards(apps, schema_editor):
-#     Flat = apps.get_model('property', 'Flat')
-#     for flat in Flat.objects.all():
-#         flat.owner_pure_phone = ''
-#         flat.save()
+def move_backwards(apps, schema_editor):
+    Flat = apps.get_model('property', 'Flat')
+    for flat in Flat.objects.all():
+        flat.owner_pure_phone = ''
+        flat.save()
 
 
 class Migration(migrations.Migration):
@@ -27,6 +27,6 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.RunPython(fill_phones)
-        # migrations.RunPython(fill_phones, move_backwards)
+        # migrations.RunPython(fill_phones)
+        migrations.RunPython(fill_phones, move_backwards)
     ]
