7aa2c8fde529a6afa3320b461c35ce1cb5e91fda
amicuswat <amicuswat@gmail.com>
2022-10-31 19:51:22 +0300

    use update instead of for

--- property/migrations/0004_auto_20221025_1557.py
+++ property/migrations/0004_auto_20221025_1557.py
@@ -4,9 +4,8 @@ from django.db import migrations
 
 def fill_new_buildings(apps, schema_editor):
     Flat = apps.get_model('property', 'Flat')
-    for flat in Flat.objects.all():
-        flat.new_building = flat.construction_year >= 2015
-        flat.save()
+    Flat.objects.filter(construction_year__lt=2015).update(new_building=False)
+    Flat.objects.filter(construction_year__gte=2015).update(new_building=True)
 
 
 class Migration(migrations.Migration):
