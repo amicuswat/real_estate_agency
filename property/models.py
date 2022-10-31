15c7649e8372a41b9ec76043eb26fcecf02d3016
amicuswat <amicuswat@gmail.com>
2022-10-31 16:07:38 +0300

    Remove Flat.owner, owner_pure_phone, owner_phonenumber
    As they duplicated Owner model fields

--- property/models.py
+++ property/models.py
@@ -5,11 +5,6 @@ from phonenumber_field.modelfields import PhoneNumberField
 
 
 class Flat(models.Model):
-    owner = models.CharField('ФИО владельца', max_length=200)
-    owner_pure_phone = PhoneNumberField(blank=True,
-                                        verbose_name='Нормализованный номер '
-                                                      'владельца')
-    owners_phonenumber = models.CharField('Номер владельца', max_length=20)
     new_building = models.BooleanField('Новостройка')
 
     created_at = models.DateTimeField(
